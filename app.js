a891fd7037c8cbfe3ad269fb37b2f83ed2291136
Tannu <tanushreebadhai@gmail.com>
2021-07-30 18:45:51 +0530

    ad

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const scrollBar = document.querySelector('#scroll-bar');
 
 window.addEventListener('scroll', () =>{
-    if (window.innerWidth > 1000){
+    if (window.innerWidth >= 1000){
     let ss = window.pageYOffset;
-    scrollBar.style.backgroundPositionY =  ss * -0.1 + "px";
+    scrollBar.style.backgroundPositionY =  ss * -0.2 + "px";
     }
 })
